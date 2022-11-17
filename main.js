941af4b985be9e22b81bb16ef4135958d39130a1
hroque01 <hadson.roque01@gmail.com>
2022-11-18 00:29:03 +0100

    Array Bomb with click

--- main.js
+++ main.js
@@ -7,20 +7,21 @@ const container = document.getElementById('container_grid')
 // the div announcement has to be deleted so we define it
 let disappeared = document.getElementById('disappeared');
 
+ // create and array of 16 bomb
+const myArrNumRandom = genArrNumRandomMinMax(16, 1, 100);
+console.log(myArrNumRandom)
+
 // now the button must work
 btn.addEventListener('click', function() {
 
     disappeared.parentNode.removeChild(disappeared);
 
     // create 100 grid using the for loop
-    for (let i = 1; i < 100; i++) {
+    for (let i = 1; i < 101; i++) {
         
-        // create and array of 100 numbers
-        // const myArrNumRandom = genArrNumRandomMinMax(100, 1, 100);
-
         // let's create the "div"
         let divElement = document.createElement("div");
-        // divElement.innerHTML += `${myArrNumRandom[i]}`
+        divElement.innerHTML = i;
 
         // now let's add the class "box" on the div
         divElement.classList.add('box');
@@ -28,38 +29,52 @@ btn.addEventListener('click', function() {
         // and add all the divs in the html. 
         container.append(divElement);
 
+        // once you click a number, if the arrNumRandom is included (it's duplicated) it's a bomb.
+        // IF you click the bomb will appear an alert with an automatic reload of the page.
+        // ELSE you can continue to play the game until you win.
+
         divElement.addEventListener('click', function(){
-            divElement.classList.toggle('clicked')
+            if (myArrNumRandom.includes(i)) {
+                alert('Ritenta')
+                location.reload();
+            } else {
+                let points = document.getElementsByClassName('clicked').length;
+                document.getElementById('title').innerHTML = `Il tuo punteggio: ${points}`;
+                divElement.classList.add('clicked');
+            }
         });
 
     }
 });
 
+
+
 // FUNCTIONS FOR GENERATE RANDOM NUMBERS
-
 // this function genetare random numbers inside the div.box
-// function randomInteger(min, max) {
-//     return ( Math.floor(Math.random()* (max + 1) - min) + min);
-// }
+function randomInteger(min, max) {
+    return ( Math.floor(Math.random()* (max + 1) - min) + min);
+}
 
-// function genArrNumRandomMinMax (howMany, minNum, maxNum) {
+function genArrNumRandomMinMax (howMany, minNum, maxNum) {
 
-//     // callback to the function
-//     let newNumber = randomInteger(minNum, maxNum);
+    // callback to the function
+    let newNumber = randomInteger(minNum, maxNum);
 
-//     // generate an array of howMany elements
-//     const arrNum = [];
+    // generate an array of howMany elements
+    const arrNum = [];
 
-//     while (arrNum.length < howMany) {
+    while (arrNum.length < howMany) {
 
-//         // define again the newNumber because the old one is outside of the while
-//         let newNumber = randomInteger(minNum, maxNum);
+        // define again the newNumber because the old one is outside of the while
+        let newNumber = randomInteger(minNum, maxNum);
 
-//         // if the newNumber is not inside of the arrNum, push it to the array
-//         if (!arrNum.includes(newNumber)) {
-//             arrNum.push(newNumber);
-//         }
-//     }
+        // if the newNumber is not inside of the arrNum, push it to the array
+        if (!arrNum.includes(newNumber)) {
+            arrNum.push(newNumber);
+        }
+    }
+
+    return arrNum;
 
 
-// }
+}
