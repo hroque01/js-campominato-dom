19b20e5601fac4e01a01681d16137365bfb304fd
hroque01 <hadson.roque01@gmail.com>
2022-11-17 21:51:26 +0100

    Grid with toggle done

--- main.js
+++ main.js
@@ -1,68 +1,65 @@
+//to define the button
+const btn = document.getElementById('button-play');
 
-// dichiaro il contenitore
-const container = document.querySelector(".grid_container");
+//define the container
+const container = document.getElementById('container_grid')
 
-// dichiaro il pulsante
-const btn = document.querySelector(".btn");
+// the div announcement has to be deleted so we define it
+let disappeared = document.getElementById('disappeared');
 
-// dichiaro la difficolta'
-const difficulty = document.getElementById('difficulty');
+// now the button must work
+btn.addEventListener('click', function() {
 
+    disappeared.parentNode.removeChild(disappeared);
 
-// il bottone deve generare la griglia
-
-btn.addEventListener('click',
-
-    function(){
-
-        container.innerHTML="";
-
-        // impostiamo la difficolta e il livello tramite griglia
-        let level = 0;
-        if (difficulty.value === "ez") {
-            level = 101;
-        };
-        if (difficulty.value === "medium") {
-            level = 82;
-        };
-        if (difficulty.value === "hard") {
-            level = 50;
-        };
+    // create 100 grid using the for loop
+    for (let i = 1; i < 100; i++) {
         
-        // ora creiamo il ciclo infinito
-        for ( let i = 1; i < level; i++ ){
+        // create and array of 100 numbers
+        // const myArrNumRandom = genArrNumRandomMinMax(100, 1, 100);
 
-              // creo l'elemento div
-            const mioElemnt = document.createElement('div');
-            mioElemnt.innerHTML += `${i}`;
-            container.append(mioElemnt);
+        // let's create the "div"
+        let divElement = document.createElement("div");
+        // divElement.innerHTML += `${myArrNumRandom[i]}`
 
-            // aggiungiamo la classe box
-            mioElemnt.classList.add('box');
+        // now let's add the class "box" on the div
+        divElement.classList.add('box');
 
-            //aggiungo la classe toggle
-            mioElemnt.addEventListener('click',
-                function(){
-                    mioElemnt.classList.toggle('clicked');
-                    console.log(i)
-                });
+        // and add all the divs in the html. 
+        container.append(divElement);
 
-            if (difficulty.value === "ez") {
-                level = 101;
-                mioElemnt.classList.add('width-easy')
-            };
-            if (difficulty.value === "medium") {
-                level = 82;
-                mioElemnt.classList.add('width-medium')
-            };
-            if (difficulty.value === "hard") {
-                level = 50;
-                mioElemnt.classList.add('width-hard')
-            };
-            
-        };
+        divElement.addEventListener('click', function(){
+            divElement.classList.toggle('clicked')
+        });
+
+    }
 });
 
+// FUNCTIONS FOR GENERATE RANDOM NUMBERS
+
+// this function genetare random numbers inside the div.box
+// function randomInteger(min, max) {
+//     return ( Math.floor(Math.random()* (max + 1) - min) + min);
+// }
+
+// function genArrNumRandomMinMax (howMany, minNum, maxNum) {
+
+//     // callback to the function
+//     let newNumber = randomInteger(minNum, maxNum);
+
+//     // generate an array of howMany elements
+//     const arrNum = [];
+
+//     while (arrNum.length < howMany) {
+
+//         // define again the newNumber because the old one is outside of the while
+//         let newNumber = randomInteger(minNum, maxNum);
+
+//         // if the newNumber is not inside of the arrNum, push it to the array
+//         if (!arrNum.includes(newNumber)) {
+//             arrNum.push(newNumber);
+//         }
+//     }
 
 
-
+// }
